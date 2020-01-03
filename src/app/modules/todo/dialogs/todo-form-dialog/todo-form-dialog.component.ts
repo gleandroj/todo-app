636a635ca249af789a6e71c15d17c24d08df89dd
Gabriel L <gabrielleandrojunior@live.com>
2020-01-03 17:59:10 -0300

    refactor(todo): link to store

--- src/app/modules/todo/dialogs/todo-form-dialog/todo-form-dialog.component.ts
+++ src/app/modules/todo/dialogs/todo-form-dialog/todo-form-dialog.component.ts
@@ -1,21 +1,43 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, Inject, OnDestroy } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { Store } from '@ngrx/store';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { TodoModel, todoPriorities } from 'src/app/models/todo.model';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ofType } from '@ngrx/effects';
+import { TodoModel, todoPriorities } from '../../../../models/todo.model';
+import * as RootStore from '../../../../../app/root-store';
+import {
+  saveTodo, selectTodoFormLoading, selectTodoFormError, TodoEffects, saveTodoSuccess
+} from '../../../../../app/root-store/todo-feature';
+import { untilDestroyed } from 'ngx-take-until-destroy';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-todo-form-dialog',
   templateUrl: './todo-form-dialog.component.html',
   styleUrls: ['./todo-form-dialog.component.scss']
 })
-export class TodoFormDialogComponent implements OnInit {
+export class TodoFormDialogComponent implements OnInit, OnDestroy {
   public form: FormGroup;
   public priorities = todoPriorities;
+  public loading$: Observable<boolean>;
+  public error$: Observable<any>;
 
   constructor(
     private fb: FormBuilder,
+    private store$: Store<RootStore.State>,
+    private todoEffects: TodoEffects,
+    public dialogRef: MatDialogRef<TodoFormDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public todo: Partial<TodoModel>
-  ) { }
+  ) {
+    this.loading$ = this.store$.select(selectTodoFormLoading);
+    this.error$ = this.store$.select(selectTodoFormError)
+      .pipe(map((error) => error && error.message));
+    this.todoEffects.saveTodo$.pipe(
+      ofType(saveTodoSuccess),
+      untilDestroyed(this)
+    ).subscribe(() => this.dialogRef.close());
+  }
 
   ngOnInit() {
     this.form = this.fb.group({
@@ -28,6 +50,20 @@ export class TodoFormDialogComponent implements OnInit {
     });
 
     this.form.get('createdAt').disable();
+
+    this.loading$
+      .pipe(untilDestroyed(this))
+      .subscribe(loading => loading ? this.form.disable() : this.form.enable());
+  }
+
+  ngOnDestroy() { }
+
+  save() {
+    const { value } = this.form;
+    const todo = TodoModel.fromObject(value);
+    this.store$.dispatch(
+      saveTodo({ todo })
+    );
   }
 
 }
